122e6e8f51896bed176f89550ca50d3d01abb6dc
polarbearmight <marius.killer15@gmail.com>
2022-09-29 15:22:36 +0200

    - class Kunde und Employee mit class Bankfiliale verbunden und 2 Methoden implementiert

--- 00_bwl/bwl/src/bwl/oo/paket8/Bankfiliale.java
+++ 00_bwl/bwl/src/bwl/oo/paket8/Bankfiliale.java
@@ -1,18 +1,37 @@
 package bwl.oo.paket8;
 
+import bwl.oo.paket10.Employee;
 import bwl.oo.paket6.Filiale;
+import bwl.oo.paket6.Kunde;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class Bankfiliale {
     public Vector<Konto> konten = new Vector<Konto>();
+    private ArrayList<Employee> employees = new ArrayList<Employee>();
+    private ArrayList<Kunde> kunde = new ArrayList<Kunde>();
     int BankID;
     String Name;
-
     public Bankfiliale(int BankID, String Name) {
         this.BankID = BankID;
         this.Name = Name;
     }
+    public void addEmployee(Employee emp) {
+        employees.add(emp);
+    }
+    public void addKunde(Kunde k) {
+        kunde.add(k);
+    }
+
+    public ArrayList<Kunde> getKunde() {
+        return kunde;
+    }
+
+    public ArrayList<Employee> getEmployees() {
+        return employees;
+    }
+
 
     public void fuegeKontoHinzu(Konto konto){
         this.konten.add(konto);
